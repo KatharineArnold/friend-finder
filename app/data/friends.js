7eca5473d6a12c5d0b36f55c026a4b97e7eec165
Katharine Arnold <katiekhs123@gmail.com>
2018-08-19 09:39:06 -0600

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,21 +2,54 @@
 let friendsList = [
 
     {
-        "name": "Jacob Deming",
-        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "name": "Trump",
+        "photo": "https://media.gq.com/photos/5ac68bdf0995d03672949444/3:2/w_880/trump-hair.jpg",
         "scores": [
-            4,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "Dot",
+        "photo": "http://secure.terrificpets.com/images/uploads/17348154541391_2.jpg",
+        "scores": [
             2,
+            1,
+            3,
+            4,
             5,
             1,
-            3,
-            2,
             2,
             1,
-            3,
+            4,
             2
         ]
-    }
+    },
+    {
+        "name": "Chopper",
+        "photo": "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001052/Basset-Hound-MP.jpg",
+        "scores": [
+            1,
+            3,
+            1,
+            1,
+            1,
+            5,
+            4,
+            4,
+            2,
+            1
+        ]
+    },
 ]
 
 module.exports = friendsList;
